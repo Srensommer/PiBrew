11373c66055b38050f1e0397bc837ea7c1c00b31
Downz <anderesmhandsome@gmail.com>
2020-02-09 22:41:24 +0100

    Right serialport + relay timing instead of calibrate for testing

--- calibrate.py
+++ calibrate.py
@@ -2,9 +2,9 @@ import serial
 import time
 
 
-ser = serial.Serial('/dev/ttyAMA0', 115200)
-command = 'c'.encode()
+ser = serial.Serial('/dev/serial0', 115200)
+command = 'rt14'.encode()
 ser.write(command)
 while not ser.in_waiting:  # Or: while self.ser.inWaiting():
-	time.sleep(1)
+	time.sleep(10)
 print(ser.readline().decode())
