5e95a06e3154d06b1f61a1344cc5ffa350f5e154
Downz <anderesmhandsome@gmail.com>
2020-02-16 13:10:17 +0100

    test

--- logMeasurements/dose_fertilizer.py
+++ logMeasurements/dose_fertilizer.py
@@ -7,8 +7,8 @@ class TimeChecker:
     def should_fertilize(self):
         t = datetime.datetime.today()
 #       if 10 > t.hour > 9 and t.minute and self.last_day_dosed != t.today():
-        if 13 < t.hour < 14 and t.minute and self.last_day_dosed != t.today():
+        if 13 < t.hour < 14 and t.minute < 15 and self.last_day_dosed != t.today():
             self.last_day_dosed = t.today()
-            print ("Fertilize")
+            print("Fertilize")
             return True
         return False
