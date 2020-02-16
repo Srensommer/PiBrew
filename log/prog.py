adb272904f1589fdccc904f542a3ef3d55099e07
Downz <anderesmhandsome@gmail.com>
2020-02-16 13:04:21 +0100

    time check

--- log/prog.py
+++ log/prog.py
@@ -1,10 +1,13 @@
 import time
 import sys
 from getValues import GetValues
+from dose_fertilizer import TimeChecker
 import requests
 
 mega = GetValues()
 mega.clear_serial_read()
+
+time_checker = TimeChecker()
 
 while True:
     water_temp = mega.get_water_temp()
@@ -12,6 +15,9 @@ while True:
     tds = mega.get_tds()
     url = 'https://hansenbrew.dk/aquarium/postautolog'
     my_data = {"temp": 30, "ph": 0, "TDS": 888}
+
+    if time_checker.should_fertilize():
+        mega.relay_timed(1, 4)
 
     x = requests.post(url, data=my_data)
 
