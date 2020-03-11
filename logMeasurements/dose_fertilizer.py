db9fc190faccbc9ee684ea6d8691ffa954f7be1f
Anders Møller Hansen <amh@iterator-it.dk>
2020-03-11 21:21:18 +0100

    Fertilizer timer fix + variables to controle debug/post mode

--- logMeasurements/dose_fertilizer.py
+++ logMeasurements/dose_fertilizer.py
@@ -4,30 +4,39 @@ from threading import Timer
 
 class TimeChecker:
     time = datetime.today()
+    # 15 min delay
+    timer_max_delay = 60*15
     
     def init(self, time):
-        print(1)
         self.time = time
-        delay_time = self.find_seconds_to_Next()
-        t = Timer(delay_time, self.fertilize)
+        self.fertilize_time_controller()
+
+    def fertilize_time_controller(self):
+        now = datetime.today()
+        next_time = now.replace(
+            day=now.day,
+            hour=self.time.hour,
+            minute=self.time.minute,
+            second=self.time.second,
+            microsecond=0
+        )
+        secs = (next_time - now).total_seconds()
+        if secs <= self.timer_max_delay:
+            if secs < 0:
+                secs += timedelta(days=1).total_seconds()
+            else:
+                t = Timer(self.timer_max_delay, self.fertilize)
+                t.start()
+                return
+
+        self.wait_longer()
+
+    def wait_longer(self):
+        print("wait longer")
+        t = Timer(self.timer_max_delay, self.fertilize_time_controller)
         t.start()
 
-
-    def find_seconds_to_Next(self):
-        print(2)
-        x = datetime.today()
-        next_time = x.replace(day=x.day, hour=self.time.hour, minute=self.time.minute, second=self.time.second, microsecond=self.time.microsecond)
-        delta_time = next_time - x
-        secs = delta_time.total_seconds()
-        if (secs < 0):
-            secs += timedelta(days=1).total_seconds()
-        print(secs)
-        return secs
-
-
     def fertilize(self):
-        print(3)
         print("Fertilize")
-        delay_time = self.find_seconds_to_Next()
-        t = Timer(delay_time, self.fertilize)
+        t = Timer(self.timer_max_delay, self.fertilize_time_controller)
         t.start()
--- logMeasurements/prog.py
+++ logMeasurements/prog.py
@@ -1,36 +1,41 @@
+debug = True
+post_to_server = False
+
 import time
 import sys
-#from getValues import GetValues
+if not debug:
+    from getValues import GetValues
 from dose_fertilizer import TimeChecker
 import requests
 from threading import Timer
 import datetime
 
-
-
-#mega = GetValues()
-#mega.clear_serial_read()
+if not debug:
+    mega = GetValues()
+    mega.clear_serial_read()
 
 time_checker = TimeChecker()
 
 
-dose_time = datetime.time(hour=18, minute=8, second=0)
+dose_time = datetime.time(hour=21, minute=9, second=0)
 time_checker.init(dose_time)
 
 while True:
-    print("new termination")
-    #water_temp = mega.get_water_temp()
-#   beer_ph = mega.get_ph()
-#   tds = mega.get_tds()
+    temp = 29
+    ph = 8
+    tds = 444
+
+    if not debug:
+        temp = mega.get_water_temp()
+        ph = mega.get_ph()
+        tds = mega.get_tds()
     url = 'https://hansenbrew.dk/aquarium/postautolog/'
-    my_data = {"temp": 30, "ph": 0, "TDS": 888}
-
+    my_data = {"temp": temp, "ph": ph, "TDS": tds}
     
-
-    print("Making Request")
-    x = requests.post(url, data=my_data)
-    print("Response:")
-    print(x.status_code)
-    if (x.status_code >= 200 and x.status_code < 300):
-        pass #log to file
+    if post_to_server:
+        print("Making Request")
+        x = requests.post(url, data=my_data)
+        print("responsecode: " + x.status_code)
+        if 200 <= x.status_code < 300:
+            pass #log to file
     time.sleep(60*10)
